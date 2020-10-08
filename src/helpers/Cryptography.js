2f0a845b4b0d961ec8650fbfb29ee3b3736ef2b8
Kevin da Silva <kevin.silva@sensedia.com>
2020-10-08 08:22:09 -0300

    Fixed random salt generator to better encrypt data

--- src/helpers/Cryptography.js
+++ src/helpers/Cryptography.js
@@ -1,12 +1,12 @@
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
+const salt = "$2b$10$k6XxkOiU99WipSPOafcuku";
 
 const generateUserCode = () => {
     return uuidv4();
 }
 
 const encryptData = async (data) => {
-    const salt = await bcrypt.genSaltSync(10);
     return await bcrypt.hashSync(data, salt);
 }
 
